b61d442e5889059470b5b315dfcce6fe81f972b4
Mohamad.mu <mhdite7@gmail.com>
2020-11-26 16:12:04 +0200

    h

--- resources/views/receipt/index.blade.php
+++ resources/views/receipt/index.blade.php
@@ -18,14 +18,14 @@
     @endif
 
 <form action="{{route('receipt.index')}}" method="get">
-    <div class="col-xs-12 col-sm-12 col-md-12">
+    <div class="col-xs-8 col-sm-8 col-md-8">
         <div class="form-group">
             <input class="form-control " type="date" name="date"/>
         </div>
 
 
     </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
+    <div class="col-xs-4 col-sm-4 col-md-4">
         <div class="form-group">
             <input class="btn btn-success" type="submit"  value="تصفية"/>
         </div>
