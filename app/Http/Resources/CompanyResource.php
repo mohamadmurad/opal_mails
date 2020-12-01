b5c90e0b25c8788488d9403c2d0178c0d364554d
Mohamad.mu <mhdite7@gmail.com>
2020-12-01 12:44:40 +0200

    dr

--- app/Http/Resources/CompanyResource.php
+++ app/Http/Resources/CompanyResource.php
@@ -19,8 +19,8 @@ class CompanyResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'logo' => asset('logo/'.$this->logo),
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at->format('Y-m-d h:m'),
+            'updated_at' => $this->updated_at->format('Y-m-d h:m'),
 
         ];
     }
