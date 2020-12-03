a493b971367d227addd4cf8634f266559a5d5324
Mohamad.mu <mhdite7@gmail.com>
2020-12-03 16:49:19 +0200

    dr

--- app/Http/Controllers/api/AuthApiController.php
+++ app/Http/Controllers/api/AuthApiController.php
@@ -78,7 +78,7 @@ class AuthApiController extends Controller
      //   dd(Auth::user());
         $user = User::where('id','=',Auth::user()->id)->first();
 
-        return  new EmployeeResource(Auth::user($user));
+        return  new EmployeeResource($user);
 
 
     }
