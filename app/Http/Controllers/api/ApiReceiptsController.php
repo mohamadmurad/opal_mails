ad086bb9a3d3b96b7cfc7cdf46d63883f9839ed8
Mohamad.mu <mhdite7@gmail.com>
2020-12-17 17:03:59 +0200

    dr

--- app/Http/Controllers/api/ApiReceiptsController.php
+++ app/Http/Controllers/api/ApiReceiptsController.php
@@ -126,10 +126,7 @@ class ApiReceiptsController extends Controller
 
     public function destroy(Request $request){
         $MyReceipt = receipts::all()->where('id','=',$request['id'])->first();
-        return response()->json([
-            'data' => $MyReceipt,
-            'code' => 400,
-        ]);
+
         $MyReceipt->delete();
 
         return response()->json([
